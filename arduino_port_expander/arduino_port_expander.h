d4bf8ddd42bb6c693b441c54d1b8d7c2a1149d42
HMEWRK.dev <duncansykes@pm.me>
2025-11-06 10:07:23 +1100

    Update arduino_port_expander.h

--- arduino_port_expander/arduino_port_expander.h
+++ arduino_port_expander/arduino_port_expander.h
@@ -9,6 +9,7 @@
 #include <vector>
 
 namespace esphome {
+namespace components {
 namespace arduino_port_expander {
 
 static const char *const TAGape = "ape";
@@ -111,6 +112,8 @@ class ArduinoPortExpander : public Component, public i2c::I2CDevice {
   std::vector<ApeSwitch *> switch_pins_;
   std::vector<ApeBinarySensor *> input_pins_;
 };
+}
+}
 
 }  // namespace arduino_port_expander
 }  // namespace esphome
